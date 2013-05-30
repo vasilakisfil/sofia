c220af38c3a77a52e675d5d371aa68c3170ddb80
Filippos Vasilakis <vasilakisfil@gmail.com>
2013-05-30 19:23:28 +0300

    Many changes (colors/content/links/photos)

--- js/maps.js
+++ js/maps.js
@@ -22,12 +22,29 @@ google.maps.event.addListener(kotsovolosMarker, 'click', function() {
 });
 
 
+var polukladikaInfowindow = new google.maps.InfoWindow({
+    content: "Πρώην Πολυκλαδικά Σχολεία Ηλιούπολης",
+    maxWidth: 200
+});
 
-var sofiaContent = "<p><strong>Σοφία Κ. Μαυρουδή</strong> \
-<span id=\"spansofiarest\"><strong>D.D.S., MSc.</strong> Ειδική Ορθοδοντικός Γναθολόγος (παθήσεις Κροτοφογναθικής - TMD) \
-Μετεκπαιδευθείσα στο Tufts University, Boston, USA</span></p>";
-sofiaContent += "<p><strong>Εθνάρχου Μακαρίου 13 και Πύρρωνος 82</strong>, <span id=\"spansofiarest\"> 16341, Ηλιούπολη. \
-Τηλέφωνο: 210 9940366 Email: orthodonticos@gmail.com</span></p>";
+var polukladikaMarker = new google.maps.Marker({
+    position: new google.maps.LatLng(37.922788,23.745961),
+    map: map,
+//     icon: {
+//       url: "js/kotsos.png",
+//       scaledSize: new google.maps.Size(90,60)
+//     },
+    zIndex: 0,
+    title: "Πρώην Πολυκλαδικά Σχολεία Ηλιούπολης"
+});
+
+google.maps.event.addListener(polukladikaMarker, 'click', function() {
+  polukladikaInfowindow.open(map,polukladikaMarker);
+});
+
+
+
+var sofiaContent = "<p><strong>Ορθοδοντικό - Γναθολογικό Ιατρείο Σ. Μαυρουδή</strong></p>";
 var iatreioInfowindow = new google.maps.InfoWindow({
     content: sofiaContent,
     maxWidth: 200
@@ -36,12 +53,12 @@ var iatreioInfowindow = new google.maps.InfoWindow({
 var iatreioMarker = new google.maps.Marker({
     position: new google.maps.LatLng(37.923965,23.746107),
     map: map,
+    zIndex: 9999,
     icon: {
-      url: "js/sofia_small.png",
+      url: "js/pink-pushpin.png",
       scaledSize: new google.maps.Size(30,35)
     },
-    zIndex: 9999,
-    title: "Ιατρείο"
+    title: "Ορθοδοντικό - Γναθολογικό Ιατρείο Σ. Μαυρουδή"
 });
 
 google.maps.event.addListener(iatreioMarker, 'click', function() {
@@ -150,13 +167,19 @@ function initialize() {
   
   // Create the DIV to hold the control of the labels
   var labelsControlDiv = document.createElement('div');
+  var fullScreenControlDiv = document.createElement('div');
 
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   
-  var labelsControl = new LabelsControl(labelsControlDiv, map);
   
+  var labelsControl = new LabelsControl(labelsControlDiv, map);
   labelsControlDiv.index = 1;
   map.controls[google.maps.ControlPosition.TOP_RIGHT].push(labelsControlDiv);
+  
+  
+  var fullScreenControl = new FullScreenControl(fullScreenControlDiv, map);
+  fullScreenControlDiv.index = 2;
+  map.controls[google.maps.ControlPosition.RIGHT_TOP].push(fullScreenControlDiv);
   
   kotsovolosMarker.setMap(map);
   iatreioMarker.setMap(map);
@@ -164,7 +187,7 @@ function initialize() {
   eisodosMarker.setMap(map);
   metroMarker.setMap(map);
   
-  SxoleiaLabel.open(map);
+  polukladikaMarker.setMap(map);
   
 }
 
@@ -172,9 +195,14 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 27) {
+     map.setCenter(new google.maps.LatLng(37.926090,23.747119));
+  }
+}, false);
 
 function LabelsControl(controlDiv, map) {
-
+  
   // Set CSS styles for the DIV containing the control
   // Setting padding to 5 px will offset the control
   // from the edge of the map
@@ -203,25 +231,79 @@ function LabelsControl(controlDiv, map) {
   // Chicago
   google.maps.event.addDomListener(controlUI, 'click', function() {
     if (LABELS) {
+      
       kotsovolosMarker.setMap(null);
       ekklhsiaMarker.setMap(null);
       eisodosMarker.setMap(null);
       metroMarker.setMap(null);      
-      SxoleiaLabel.close();     
+      polukladikaMarker.setMap(null);    
       LABELS = false;
-
       document.getElementById('labelscontrol').innerHTML ='Εμφάνιση ετικετών';
 
+      
     } else {
       kotsovolosMarker.setMap(map);
       ekklhsiaMarker.setMap(map);
       eisodosMarker.setMap(map);
       metroMarker.setMap(map);
-      SxoleiaLabel.open(map);  
+      polukladikaMarker.setMap(map);
       LABELS = true;
-      
       document.getElementById('labelscontrol').innerHTML ='Απόκρυψη ετικετών';
     }
+  });
+
+}
+
+
+function FullScreenControl(controlDiv, map) {
+  
+  // Set CSS styles for the DIV containing the control
+  // Setting padding to 5 px will offset the control
+  // from the edge of the map
+  controlDiv.style.padding = '5px';
+  
+  
+  // Set CSS for the control border
+  var FullScreenUI = document.createElement('div');
+  FullScreenUI.style.backgroundColor = 'white';
+  FullScreenUI.style.borderStyle = 'solid';
+  FullScreenUI.style.borderWidth = '2px';
+  FullScreenUI.style.cursor = 'pointer';
+  FullScreenUI.style.textAlign = 'center';
+  FullScreenUI.title = 'Πατήστε για πλήρη οθόνη';
+  controlDiv.appendChild(FullScreenUI);
+
+  // Set CSS for the control interior
+  var FullScreenText = document.createElement('div');
+  FullScreenText.style.fontFamily = 'Arial,sans-serif';
+  FullScreenText.style.fontSize = '12px';
+  FullScreenText.style.paddingLeft = '4px';
+  FullScreenText.style.paddingRight = '4px';
+  FullScreenText.innerHTML = '<strong id=\"FullScreenControl\">FullScreen</strong>';
+  FullScreenUI.appendChild(FullScreenText);
+
+  // Setup the click event listeners: simply set the map to
+  // Chicago
+  google.maps.event.addDomListener(FullScreenUI, 'click', function() {
+
+      
+    var elem = document.getElementById("map-canvas");
+    elem.className = "fullscreen";
+    
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    }
+    
+    
+//     var myLatlng = new google.maps.LatLng(37.923636,23.725837);
+//     map.setCenter(new google.maps.LatLng(37.923636,23.725837));
+// 
+//     document.getElementById('FullScreenControl').innerHTML ='Exit FullScreen';
+
   });
 
 }
